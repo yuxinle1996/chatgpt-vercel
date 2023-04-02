302693f3221f6e98a15415bc8047be0dae9f3101
大咪 <1379605323@qq.com>
2023-04-02 23:28:33 +0800

    密码错误

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -84,7 +84,7 @@ export const post: APIRoute = async context => {
     } = body
 
     if (pwd && pwd !== password) {
-      throw new Error("密码错误，请联系网站管理员。")
+      throw new Error("密码错误，请联系大咪。qq1379605323")
     }
 
     if (!messages?.length) {
